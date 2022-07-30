bcc137a49c328c0a921fe62669e37d51bb6253ad
Hassaan Ali <hassaanafzal789@gmail.com>
2022-07-30 20:26:15 +0500

    TestController Added

---
+++ OnlineTicketManagement/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineTicketManagement.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
